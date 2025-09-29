2cc736df8ae4b93531914d6599fd9c6b30fe11b9
ITB2526-TA02-PabloPineda <pablo.pineda.7e7@itb.cat>
2025-09-29 12:42:06 +0200

    Commit final (Practica pulida)

--- ComprobaredadPPM.py
+++ ComprobaredadPPM.py
@@ -1,6 +1,16 @@
+# Demanem a l'usuari que introdueixi la seva edat
 edat = int(input("Quina edat tens? "))
 
+# Comprovem si l'usuari és major d'edat
 if edat >= 18:
-    print("Ets major d'edat,programa finalitzat")
+    print("Ets major d'edat,programa finalitzat")  # Missatge si és major d'edat
 else:
-    print("No ets major d'edat,programa finalitzat")
+    print("No ets major d'edat,programa finalitzat")  # Missatge si no és major d'edat
+
+"""
+ASIXC1-A
+Pablo Leonel Pineda Martinez
+29/09/2025
+⧉⟦ MDS · Llenguatge ∴ Sostenibilitat ∴ Digitalització ⟧⧉
+# Fi del programa
+"""
--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("hello world")
+# Este es mi primer programa en Python
+# Imprime un mensaje de saludo en la consola
+# Mensaje que vamos a mostrar
+
+print("*Hello, World!*")
+"""
+ASIXC1-A
+Pablo Leonel Pineda Martinez
+29/09/2025
+⧉⟦ MDS · Llenguatge ∴ Sostenibilitat ∴ Digitalització ⟧⧉
+"""
+# Fin del programa
